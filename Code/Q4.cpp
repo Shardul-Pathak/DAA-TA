7e8adc6b8edc5dccc83de3df0362c9b36ddf22b3
Shardul Pathak <shardulpathak796@gmail.com>
2025-09-15 23:00:59 +0530

    Initial Commit

--- Code/Q4.cpp
+++ Code/Q4.cpp
@@ -13,7 +13,7 @@ void bellmanFord(int V, int E, vector<Edge>& edges, int source) {
     vector<int> dist(V, INF);
     dist[source] = 0;
 
-    for (int i = 0; i < V - 1; ++i) {
+    for (int i = 0; i < V - 1; i++) {
         for (auto& edge : edges) {
             if (dist[edge.u] != INF && dist[edge.u] + edge.weight < dist[edge.v]) {
                 dist[edge.v] = dist[edge.u] + edge.weight;
@@ -29,7 +29,7 @@ void bellmanFord(int V, int E, vector<Edge>& edges, int source) {
     }
 
     cout << "Shortest distances from source " << source << ":" << endl;
-    for (int i = 0; i < V; ++i) {
+    for (int i = 0; i < V; i++) {
         cout << "Node " << i << ": ";
         if (dist[i] == INF) {
             cout << "Unreachable" << endl;
@@ -40,29 +40,20 @@ void bellmanFord(int V, int E, vector<Edge>& edges, int source) {
 }
 
 int main() {
-    int V = 11;
+    int V = 11,E;
+    cout << "Enter the number of edges: ";
+    cin >> E;
     vector<Edge> edges;
+    cout << "Enter the edges:" << endl;
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, w});
+    }
+    int sourceNode;
+    cout << "Enter the source node: ";
+    cin >> sourceNode;
 
-    edges.push_back({0, 1, 6});
-    edges.push_back({0, 2, 7});
-    edges.push_back({1, 3, 5});
-    edges.push_back({1, 4, -4});
-    edges.push_back({2, 4, 9});
-    edges.push_back({2, 5, 8});
-    edges.push_back({3, 6, -2});
-    edges.push_back({4, 6, 7});
-    edges.push_back({4, 7, 1});
-    edges.push_back({5, 7, -3});
-    edges.push_back({6, 8, 4});
-    edges.push_back({7, 8, 2});
-    edges.push_back({7, 9, 3});
-    edges.push_back({8, 10, 5});
-    edges.push_back({9, 10, -1});
-
-    int E = edges.size();
-    int source_node = 0;
-
-    bellmanFord(V, E, edges, source_node);
-
+    bellmanFord(V, E, edges, sourceNode);
     return 0;
 }
